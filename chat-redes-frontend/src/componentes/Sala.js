8ad5539fc4007596a1c9847246f173779f63ac94
Eduardo <eduardoarmando.palma@userena.cl>
2022-06-26 15:22:01 -0400

    Update front-end

--- chat-redes-frontend/src/componentes/Sala.js
+++ chat-redes-frontend/src/componentes/Sala.js
@@ -98,23 +98,29 @@ const Sala = () => {
 				</div>
 				<div className='table-mensajes'> 
 						<div className='mensajes-publicos'>
-							{salaPublica.map((chatMensaje,index) => (
-								<div className={`mensaje ${chatMensaje.nombreRemitente === datosUsuario.nombreUsuario && "usuario"}`} key={index}>
-									<div className='dato-remitente'>{chatMensaje.nombreRemitente}</div>
-									<div className='dato-mensaje'>{chatMensaje.mensaje}</div>
-								</div>
-							))}	
+								{salaPublica.map((chatMensaje,index) => (
+									<div className={`contenedor-mensaje ${chatMensaje.nombreRemitente === datosUsuario.nombreUsuario && "left"}`}>
+										<div className={`mensaje ${chatMensaje.nombreRemitente === datosUsuario.nombreUsuario && "usuario"}`} key={index}>
+											<div className='dato-remitente'>{chatMensaje.nombreRemitente}</div>
+											<div className='dato-mensaje'>{chatMensaje.mensaje}</div>
+										</div>
+									</div>
+								))}	
 						</div>
 						<div className='enviar-mensaje'> 
 							<input className='entrada-mensaje' type="text" placeholder='ingrese un Mensaje' value={datosUsuario.mensaje} onChange={manejarMensaje}></input>
-							<button classname ="btn btn-success" type='button' onClick={enviarMensaje} >Enviar</button>
+							<button type='button' onClick={enviarMensaje} >
+								<svg xmlns="http://www.w3.org/2000/svg" width="20" height="13" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+									<path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
+								</svg>
+							</button>
 						</div>
 				</div>
 			</div>
 			:
 			<div className='entrada-usuario'>
 				<input id='nombre-usuario' placeholder='ingrese su nombre' value={datosUsuario.nombreUsuario} onChange = {manejarUsuario}></input>
-				<button className="" type='button' onClick={conectado}> Conectarse</button>
+					<button type='button' onClick={conectado}> Conectarse</button>
 			</div>
 			}
 		</div>
